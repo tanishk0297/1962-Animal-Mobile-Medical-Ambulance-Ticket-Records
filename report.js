6f4422e6a2cce276fee4bbf61d28e048b831c0a3
tanishk0297 <tanishk0297@gmail.com>
2024-10-07 18:10:31 +0530

    report date

--- report.js
+++ report.js
@@ -6,7 +6,7 @@ const base3 = new Airtable({ apiKey: 'pat9fREdITpFW3UdB.13d5c2b0a2e5a4316b7124d3
 const base4 = new Airtable({ apiKey: 'pat9fREdITpFW3UdB.13d5c2b0a2e5a4316b7124d354081bd11ced915241a18dc56a5b913501127ef2' }).base('appym1xh8nuBogY9r');
 const base5 = new Airtable({ apiKey: 'pat9fREdITpFW3UdB.13d5c2b0a2e5a4316b7124d354081bd11ced915241a18dc56a5b913501127ef2' }).base('appna8sgnyyzTFRtL');
 const base6 = new Airtable({ apiKey: 'pat9fREdITpFW3UdB.13d5c2b0a2e5a4316b7124d354081bd11ced915241a18dc56a5b913501127ef2' }).base('appDb3Dttu2fjJl1b');
-const base7 = new Airtable({ apiKey: 'pat9fREdITpFW3UdB.13d5c2b0a2e5a4316b7124d354081bd11ced915241a18dc56a5b913501127ef2' }).base('appDb3Dttu2fjJl1b');
+const base7 = new Airtable({ apiKey: 'pat9fREdITpFW3UdB.13d5c2b0a2e5a4316b7124d354081bd11ced915241a18dc56a5b913501127ef2' }).base('apppQcGGzTZKVtpmG');
 
 const vehicleDetails = {
     "1": { number: "MP-02-ZA-0104", location: "केसली" },
@@ -39,7 +39,7 @@ function getBase(selectedDate) {
     const cutoffDate3 = new Date('2024-07-22');
     const cutoffDate4 = new Date('2024-08-16');
     const cutoffDate5 = new Date('2024-09-10');
-    const cutoffDate6 = new Date('2024-10-05');
+    const cutoffDate6 = new Date('2024-10-06');
     const date = new Date(selectedDate);
 
     if (date >= cutoffDate6) {
